c120af1a4acf2845a0e0c8fa6268960586703e29
claterveer <charlielaterveer7@gmail.com>
2025-03-19 20:12:39 +1300

    I added:
    actual login functionality

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect, session
 import sqlite3
 from sqlite3 import Error
+from flask_bcrypt import Bcrypt
 
 DATABASE = 'sessions'
 app = Flask(__name__)
-app.secret_key = "secret_key"
+app.secret_key = "digitechdatasecretkey"
+bcrypt = Bcrypt(app)
 def connect_database(db_file):
     try:
         con = sqlite3.connect(db_file)
@@ -28,6 +30,8 @@ def render_signup_page():
         if len(password) < 8:
             return redirect("\signup?error=password+must+at+least+8+characters")
 
+        hashed_password = bcrypt.generate_password_hash(password)
+
         con = connect_database(DATABASE)
         cur = con.cursor()
         query1 = "SELECT email FROM user"
@@ -36,44 +40,38 @@ def render_signup_page():
         if (email,) in all_emails:
             return redirect('\signup?error=email+already+associated+with+an+account')
         query_insert = "INSERT INTO user (fname, lname, email, password) VALUES (?, ?, ?, ?)"
-        cur.execute(query_insert, (fname, lname, email, password))
+        cur.execute(query_insert, (fname, lname, email, hashed_password))
         con.commit()
         con.close()
+        return render_template('login.html')
     return render_template('signup.html')
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def render_login_page():
     if request.method == 'POST':
+        email = request.form.get('user_email').lower().strip()
+        password = request.form.get('user_password')
+        query1 = "SELECT user_id, fname, password FROM user WHERE email = ?"
         con = connect_database(DATABASE)
         cur = con.cursor()
-        query1 = "SELECT user_id, email, password, fname FROM user"
-        cur.execute(query1)
-        all_emails = cur.fetchall()
+        cur.execute(query1, (email,))
         user_info = cur.fetchone()
         con.close()
-        if len(all_emails) > 0:
-            email = request.form.get('user_email').lower().strip()
-            password = request.form.get('user_password')
-            email_found = False
-            for i, x in enumerate(all_emails):
-                if email in x:
-                    email_location = all_emails[i]
-                    email_found = True
-            if email_found == True:
-                if password == email_location[2]:
-                    session['user_id'] = user_info[0]
-                    session['email'] = user_info[1]
-                    session['fname'] = user_info[3]
+        try:
+            user_id = user_info[0]
+            fname = user_info[3]
+            user_password = user_info[2]
+        except IndexError:
+            return redirect('\login?error=email+or+password+is+invalid')
 
-                else:
-                    return redirect('\signup?error=passwords+do+not+match')
-            else:
-                return redirect('\signup?error=email+not+associated+with+an+account')
-        else:
-            return redirect('\signup?error=email+not+associated+with+an+account')
+        if not bcrypt.check_password_hash(user_password, password):
+            return redirect('\login?error=email+or+password+is+invalid')
 
-
+        session['email'] = email
+        session['user_id'] = user_id
+        session['fname'] = fname
+        return redirect('/')
     return render_template('login.html')
 
 @app.route('/')
@@ -82,11 +80,19 @@ def hello_world():  # put application's code here
 
 @app.route('/my_sessions')
 def render_sessions():  # put application's code here
+
     return render_template('my_sessions.html')
 
 @app.route('/create_sessions')
 def render_create_sessions():
-    return render_template('create_sessions.html')
+    if request.method == 'GET':
+        con = connect_database(DATABASE)
+        cur = con.cursor()
+        query = "SELECT * FROM my_sessions"
+        cur.execute(query)
+        l = cur.fetchall()
+        con.close()
+    return render_template('create_sessions.html', loco = l)
 
 @app.route('/schedule')
 def render_schedule():
