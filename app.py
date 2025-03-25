2c816aadbfd7bce9cbce5f216bd014c4376f8905
claterveer <charlielaterveer7@gmail.com>
2025-03-25 19:22:00 +1300

    I added:
    addable sessions
    currently working on adding classroom sessions

--- app.py
+++ app.py
@@ -29,9 +29,7 @@ def render_signup_page():
 
         if len(password) < 8:
             return redirect("\signup?error=password+must+at+least+8+characters")
-
-        hashed_password = bcrypt.generate_password_hash(password)
-
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
         con = connect_database(DATABASE)
         cur = con.cursor()
         query1 = "SELECT email FROM user"
@@ -39,7 +37,7 @@ def render_signup_page():
         all_emails = cur.fetchall()
         if (email,) in all_emails:
             return redirect('\signup?error=email+already+associated+with+an+account')
-        query_insert = "INSERT INTO user (fname, lname, email, password) VALUES (?, ?, ?, ?)"
+        query_insert = "INSERT INTO user (fname, lname, email, hashed_password) VALUES (?, ?, ?, ?)"
         cur.execute(query_insert, (fname, lname, email, hashed_password))
         con.commit()
         con.close()
@@ -49,60 +47,135 @@ def render_signup_page():
 
 @app.route('/login', methods=['POST', 'GET'])
 def render_login_page():
+    if 'user_id' in session:
+        print(session)
+        return redirect('/logout')
+    else:
+        if request.method == 'POST':
+            email = request.form.get('user_email').lower().strip()
+            hashed_password = request.form.get('tutee_password')
+            tutee_hash_password = bcrypt.generate_password_hash(hashed_password).decode('utf-8')
+            query1 = "SELECT user_id, fname, password FROM user WHERE email = ?"
+            con = connect_database(DATABASE)
+            cur = con.cursor()
+            cur.execute(query1, (email,))
+            user_info = cur.fetchone()
+            con.close()
+            try:
+                print(user_info)
+                user_id = user_info[0]
+                fname = user_info[1]
+                user_password = user_info[2]
+            except IndexError:
+                print('Index Error')
+                return redirect('\login?error=email+or+password+is+invalid')
+
+            if hashed_password != user_password:
+                print(hashed_password)
+                print(user_password)
+                print('Hash Error')
+                return redirect('\login?error=email+or+password+is+invalid')
+            else:
+                session['email'] = email
+                session['user_id'] = user_id
+                session['fname'] = fname
+                return redirect('/')
+        return render_template('login.html')
+
+@app.route('/logout', methods=['POST', 'GET'])
+def render_logout():
     if request.method == 'POST':
-        email = request.form.get('user_email').lower().strip()
-        password = request.form.get('user_password')
-        query1 = "SELECT user_id, fname, password FROM user WHERE email = ?"
-        con = connect_database(DATABASE)
-        cur = con.cursor()
-        cur.execute(query1, (email,))
-        user_info = cur.fetchone()
-        con.close()
-        try:
-            user_id = user_info[0]
-            fname = user_info[3]
-            user_password = user_info[2]
-        except IndexError:
-            return redirect('\login?error=email+or+password+is+invalid')
-
-        if not bcrypt.check_password_hash(user_password, password):
-            return redirect('\login?error=email+or+password+is+invalid')
-
-        session['email'] = email
-        session['user_id'] = user_id
-        session['fname'] = fname
-        return redirect('/')
-    return render_template('login.html')
-
+        session.pop('email')
+        session.pop('user_id')
+        session.pop('fname')
+        print(session)
+        return redirect('/login')
+    return render_template('logout.html')
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('index.html')
 
-@app.route('/my_sessions')
-def render_sessions():  # put application's code here
+@app.route('/my_sessions', methods=['GET', 'POST'])
+def render_sessions():
+    if 'user_id' not in session:
+        return redirect('/login')
+    user_id = session['user_id']
+    try:
+        con = connect_database(DATABASE)  # connect to database
+        query = f"SELECT location, date, title, description, start_time, end_time FROM my_sessions WHERE user_id = ?"
+        cur = con.cursor()
+        cur.execute(query, (user_id, ))
+        session_list = cur.fetchall()
+        print(session_list)
+        con.close()
+        return render_template('my_sessions.html', list_of_sessions=session_list, user_id=user_id)
+    except Error as e:
+        print('Error')
+        return render_template('my_sessions.html', error=f"An error occurred: {e}")
 
-    return render_template('my_sessions.html')
-
-@app.route('/create_sessions')
+@app.route('/create_sessions', methods=['POST', 'GET'])
 def render_create_sessions():
+    if request.method == 'POST':
+        s_location = request.form.get('location')
+        s_date = request.form.get('date')
+        s_start_time = request.form.get('start_time')
+        s_end_time = request.form.get('end_time')
+        s_tute_id = session['user_id']
+        try:
+            con = connect_database(DATABASE)
+            cur = con.cursor()
+            check_tutee_query = "SELECT * FROM user WHERE user_id = ?"
+            cur.execute(check_tutee_query, (s_tute_id,))
+            tutee = cur.fetchone()
+            if not tutee:
+                con.close()
+                error_message = "TUTEE DOES NOT EXIST"
+                return render_template('create_sessions.html', error=error_message)
+            check_session_query = "SELECT * FROM my_sessions WHERE user_id = ? AND date = ? AND location = ? AND start_time = ? AND end_time = ?"
+            cur.execute(check_session_query, (s_tute_id, s_date, s_location, s_start_time, s_end_time))
+            existing_session_list = cur.fetchall()
+            if existing_session_list:
+                con.close()
+                return render_template('create_sessions.html', error=f"session already exists")
+            insert_session_query = "INSERT INTO my_sessions (location, date, title, description, start_time, end_time, user_id) VALUES (?, ?, ?, ?, ?, ?, ?)"
+            cur.execute(insert_session_query, (s_location, s_date, 'Tutoring Session', 'Session with Charlie Laterveer', s_start_time, s_end_time, s_tute_id,))
+            print('Success')
+            con.commit()
+            con.close()
+            return redirect('/my_sessions')
+        except Error as e:
+            print(f"Error occurred: {e}")
+            return render_template('create_sessions.html', error=f"An error occurred: {e}")
+    return render_template('create_sessions.html')
+
+@app.route('/schedule', methods=['POST', 'GET'])
+def render_schedule():
     if request.method == 'GET':
         con = connect_database(DATABASE)
+        query = "SELECT Date, Time, end_time, Title, Description, Location FROM schedule"
         cur = con.cursor()
-        query = "SELECT * FROM my_sessions"
         cur.execute(query)
-        l = cur.fetchall()
-        con.close()
-    return render_template('create_sessions.html', loco = l)
-
-@app.route('/schedule')
-def render_schedule():
-    con = connect_database(DATABASE)
-    query = "SELECT Date, Time, end_time, Title, Description, Location FROM schedule"
-    cur = con.cursor()
-    cur.execute(query)
-    schedule_list = cur.fetchall()
-    print(schedule_list)
-    return render_template('schedule.html', sched=schedule_list)
+        schedule_list = cur.fetchall()
+        print(schedule_list)
+        return render_template('schedule.html', sched=schedule_list)
+    elif request.method == 'POST':
+        Location = request.form.get('Location')
+        Date = request.form.get('Date')
+        Title = request.form.get('Title')
+        print(Title)
+        Description = request.form.get('Description')
+        Time = request.form.get('Time')
+        end_time = request.form.get('end_time')
+        user_id = session['user_id']
+        try:
+            con = connect_database(DATABASE)
+            cur = con.cursor()
+            insert_session_query = "INSERT INTO my_sessions (location, date, title, description, start_time, end_time, user_id) VALUES (?, ?, ?, ?, ?, ?, ?) "
+            cur.execute(insert_session_query, (Location, Date, Title, Description, Time, end_time, user_id))
+        except Error as e:
+            print(f"Error occurred: {e}")
+            return render_template('schedule.html', error=f"An error occurred: {e}")
+    return redirect('/my_sessions')
 
 
 
